fb6e74aff613b9104f2c528826399bd1a4755c89
Niole Nelson <niolenelson@gmail.com>
2016-09-05 20:46:39 -0700

    creates state tracker and refactors everything to use this and have simple api

--- app/Draggable.jsx
+++ app/Draggable.jsx
@@ -1,11 +1,18 @@
 import React, {PropTypes, Component} from 'react';
 
-const { object } = PropTypes;
+const { bool, object, func } = PropTypes;
 const propTypes = {
-  style: object
+  style: object,
+  onDragStyle: object,
+  dragHandleStyle: object,
+  onDrag: func.isRequired,
+  onDragEnd: func.isRequired,
+  isDragging: bool.isRequired
 };
 const defaultProps = {
-  style: {
+  style: {},
+  onDragStyle: {},
+  dragHandleStyle: {
     left: 0,
     top: 0,
     width: 10,
@@ -17,17 +24,21 @@ export default class Draggable extends Component {
   render() {
     const {
       style,
-      onDragStart,
+      onDragStyle,
+      dragHandleStyle,
+      onDrag,
       onDragEnd,
+      isDragging,
       children
     } = this.props;
 
     return (
-      <div
-          style={ style }
-          draggable="true"
-          onDrag={ onDragStart }
-          onDragEnd={ onDragEnd }>
+      <div style={ isDragging ? onDragStyle : style }>
+        <div
+            style={ dragHandleStyle }
+            draggable="true"
+            onDrag={ onDrag }
+            onDragEnd={ onDragEnd }/>
         { children }
       </div>
     );
